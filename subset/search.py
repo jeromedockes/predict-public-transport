e635485752539ce3a1c7f3088c33aaf027ba3415
Jerome Dockes <jerome@dockes.org>
2025-01-14 11:25:14 +0100

    _

--- subset/search.py
+++ subset/search.py
@@ -14,11 +14,12 @@ search = pred.skb.get_randomized_search(
     n_jobs=8,
 )
 search.fit({"data": usage})
-print(search.get_cv_results_table())
-search.plot_parallel_coord().show()
 
 with open("search-model.pickle", "wb") as stream:
     pickle.dump(search, stream)
 
 with open("best-model.pickle", "wb") as stream:
     pickle.dump(search.best_estimator_, stream)
+
+print(search.get_cv_results_table())
+search.plot_parallel_coord().show()
